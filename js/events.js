e4fb9f45357361f19ad08a775f7bdd1980a78263
Zachary Coursey <zcoursey22@gmail.com>
2018-02-06 18:42:40 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -15,7 +15,7 @@ function frameIt() {
 
 function pressIt() {
   $('input').on('keydown', function(key) {
-    if (key.which == 70) {
+    if (key.which == 71) {
       alert('g was pressed');
     }
   });
